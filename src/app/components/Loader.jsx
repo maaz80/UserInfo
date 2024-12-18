a3777e70c433534ca837857720d5e186f635ce24
maaz80 <maazforlap@gmail.com>
2024-12-18 19:55:07 +0530

    finished

--- src/app/components/Loader.jsx
+++ src/app/components/Loader.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Spinner from "./Spinner.gif"
+import Image from 'next/image';
 
 const Loader = () => (
   <div className='w-full h-[700px] flex justify-center items-center'>
-  <img src={Spinner} alt="Spinner" className='flex justify-center items-center' />
+  <Image src={Spinner} alt="Spinner" className='flex justify-center items-center' />
   </div>
 );
 
